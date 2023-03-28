bfd252455f30f08c21e857945c5c1d2745c51875
Eva Simonyi <Eva_Simonyi@epam.com>
2023-03-28 22:31:08 +0200

    clean up server

--- BE/server.js
+++ BE/server.js
@@ -17,41 +17,15 @@ db.once('open', function () {
   console.log('Connected successfully');
 });
 
-app.post('/api/movie', async (request, response) => {
-  const movie = new movieModel(request.body);
-
-  try {
-    await movie.save();
-    response.send(movie);
-  } catch (error) {
-    response.status(500).send(error);
-  }
-});
-
 app.get('/api/movies', async (request, response) => {
-  console.log(movieModel);
   const movies = await movieModel.find();
-  console.log(movies);
+
   try {
     response.send(movies);
   } catch (error) {
     response.status(500).send(error);
   }
 });
-
-// app.get('/api/movies', function (request, response) {
-//   let res = {
-//     metadata: {
-//       status: 200,
-//       message: null,
-//     },
-//     data: movies,
-//   };
-//   response
-//     .setHeader('content-type', 'application/json')
-//     .status(res.metadata.status)
-//     .send(res);
-// });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
